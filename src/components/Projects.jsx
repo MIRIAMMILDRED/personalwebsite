ca87238d1a0d5049d1f61bc16a915716df355495
MIRIAMMILDRED <miriammidred20@gmail.com>
2024-09-17 18:02:36 +0300

    updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import nova from '../assets/projects/novaapp.png';
 import maxbet from '../assets/projects/maxbetmx.png';
 import restaurant from '../assets/projects/restaurant.png';
-import leadsoko from '../assets/projects/leadsoko_home.png';
+// import leadsoko from '../assets/projects/leadsoko_home.png';
 
 const Projects = () => {
   const projects = [
@@ -30,14 +30,14 @@ const Projects = () => {
       link: "https://miriammildred.github.io/restaurant/",
       github: "https://github.com/MIRIAMMILDRED/restaurant",
     },
-    {
-      title: "LeadSoko Digital Agency",
-      description:
-        "A leading digital marketing Agency that helps you to grow your bussiness and brand utilizing technology. ",
-      photo: leadsoko,
-      link: "https://leadsoko.com/",
-      // github: "https://github.com/miriammildred/trivia",
-    },
+    // {
+    //   title: "LeadSoko Digital Agency",
+    //   description:
+    //     "A leading digital marketing Agency that helps you to grow your bussiness and brand utilizing technology. ",
+    //   photo: leadsoko,
+    //   // link: "https://leadsoko.com/",
+    //   // github: "https://github.com/miriammildred/trivia",
+    // },
    
     
     
